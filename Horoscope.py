9ebdc7eac9889986cce77b4f7644922de46b6ffd
ThaHolyPaladin <65267796+ThaHolyPaladin@users.noreply.github.com>
2020-05-19 15:02:17 -0700

    Update 2

--- Horoscope.py
+++ Horoscope.py
@@ -3,8 +3,14 @@ question = (input("Hello! I hope you are having a good day. Are you ready to get
 if question == 'Yes' or question == 'yes':
     month = (input("Yay! What is your birth month?\n"))
 
-    if month == "july" or "July" or "august" or "August":
+    if month == "july" or "July":
         day = (input("What is your day of birth?\n"))
+        	if day == range(1, 22)
+        	print ("Your sign is cancer!")
+
+        else:
+        	if day == range(23, 31)
+        	print ("Your sign is Leo!") 
 
 elif question == 'No' or question == 'no':
     print("Boo!")
